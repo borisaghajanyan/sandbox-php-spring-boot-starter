73ac0c16d6cd1c7d8522eca1619eb6be66012909
borisaghajanyan <boris.aghajanyan@gmail.com>
2026-01-14 16:58:09 +0100

    Fixing the place of resources

--- src/main/java/com/baghajanyan/sandbox/php/config/PhpSandboxAutoConfiguration.java
+++ src/main/java/com/baghajanyan/sandbox/php/config/PhpSandboxAutoConfiguration.java
@@ -4,17 +4,18 @@ import java.util.concurrent.Semaphore;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-import com.baghajanyan.sandbox.core.executor.CodeExecutor;
 import com.baghajanyan.sandbox.php.executor.PhpCodeExecutor;
 
 @AutoConfiguration
+@EnableConfigurationProperties(PhpSandboxProperties.class)
 public class PhpSandboxAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public CodeExecutor phpCodeExecutor(PhpSandboxProperties properties) {
+    public PhpCodeExecutor phpCodeExecutor(PhpSandboxProperties properties) {
         return new PhpCodeExecutor(new Semaphore(properties.getMaxConcurrency()));
     }
 }
